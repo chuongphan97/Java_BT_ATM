ba43e6b356720f70f9f709c386429687aeb70ff0
pgtc97 <79627755+pgtc97@users.noreply.github.com>
2021-04-14 15:48:41 +0700

    update

---
+++ ATM/src/ATMAccount.java
@@ -0,0 +1,168 @@
+import java.util.IllegalFormatCodePointException;
+import java.util.Scanner;
+
+public class ATMAccount {
+    private String name;
+    private double balance;
+    private String[] history = new String[3];
+
+    public ATMAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public ATMAccount() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void openMenu(){
+        System.out.println("Menu: ");
+        System.out.println();
+        System.out.println("\t 1. Press A to check your account.");
+        System.out.println("\t 2. Press D to deposit.");
+        System.out.println("\t 3. Press W to withdraw.");
+        System.out.println("\t 4. Press H to view History");
+        System.out.println("\t 5. Press X to exit.");
+    }
+
+    public void checkAccount(){
+        System.out.println("Your balance: "+balance+"$");
+        System.out.println();
+        System.out.println("Enter your choice: ");
+        System.out.println("==================================");
+        openMenu();
+    }
+
+    public void deposit(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Deposit transaction: ");
+        System.out.println("Please enter your money: ");
+        double amount = sc.nextDouble();
+        System.out.println("Successful transaction!. You was deposited " + amount+ "$ successfully");
+        setBalance(balance+amount);
+        System.out.println("Your balance: "+balance+"$");
+
+        if (history[0] == null && history[1] == null && history[2] == null){
+            history[0] = "Deposit "+amount+"$.";
+        } else if (history[1] == null && history[2] == null){
+            history[1] = "Deposit "+amount+"$.";
+        }       else if (history[2] == null){
+                    history[2] = "Deposit "+amount+"$.";
+        }           else {
+                        history[0] = history[1];
+                        history[1] = history[2];
+                        history[2] = "Deposit: "+ amount+"$.";
+        }
+
+        System.out.println();
+        System.out.println("Enter your choice: ");
+        System.out.println("==================================");
+        openMenu();
+    }
+
+    public void withdraw(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Withdraw transaction: ");
+        System.out.println("Please enter your money: ");
+        double amount = sc.nextDouble();
+        if (amount > this.getBalance()) {
+            System.out.println("Transaction was unsuccessful!");
+            System.out.println("Your balance: "+this.getBalance()+"$");
+            System.out.println("Your balance is not enough to withdraw!");
+            System.out.println();
+            System.out.println("Enter your choice: ");
+            System.out.println("==================================");
+            openMenu();
+        }   else {
+            System.out.println("Successful transaction!. You was withdrawn " + amount+ "$ successfully");
+            setBalance(balance - amount);
+            System.out.println("Your balance: "+balance+"$");
+
+            if (history[0] == null && history[1] == null && history[2] == null){
+                history[0] = "Withdraw: "+amount+"$.";
+            } else if (history[1] == null && history[2] == null){
+                history[1] = "Withdraw: "+amount+"$.";
+            }       else if (history[2] == null){
+                        history[2] = "Withdraw: "+amount+"$.";
+            }           else {
+                            history[0] = history[1];
+                            history[1] = history[2];
+                            history[2] = "Withdraw: "+ amount+"$.";
+            }
+
+            System.out.println();
+            System.out.println("Enter your choice: ");
+            System.out.println("==================================");
+            openMenu();
+        }
+    }
+
+    public void openHistory(){
+        System.out.println("History transaction: ");
+        for (int i = 2; i >= 0; i--) {
+            if (history[i] != null) {
+                System.out.println(3-i+". "+ history[i]);
+            } else {
+                System.out.println("Empty!");
+                break;
+            }
+
+        }
+        System.out.println();
+        System.out.println("Enter your choice: ");
+        System.out.println("==================================");
+        openMenu();
+    }
+
+    public void login(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome "+name+" to ABC Bank!");
+        openMenu();
+        System.out.println("==================================");
+        System.out.println("Enter your choice: ");
+
+        String choice = sc.nextLine();
+        while (!choice.equals("X")) {
+            if (choice.equals("A")) {
+                checkAccount();
+                choice =sc.nextLine();
+            } else if (choice.equals("W")) {
+                withdraw();
+                choice = sc.nextLine();
+            }   else if (choice.equals("D")){
+                deposit();
+                choice = sc.nextLine();
+            }       else if (choice.equals("H")){
+                openHistory();
+                choice = sc.nextLine();
+            }           else {
+                System.out.println("Invalid choice!");
+                System.out.println("Enter your choice: ");
+                System.out.println("==================================");
+                openMenu();
+                choice = sc.nextLine();
+            }
+        }
+        if (choice.equals("X")){
+            System.out.println("Thanks for using our ATM");
+        }
+    }
+
+
+
+}
---
+++ ATM/src/ATMTest.java
@@ -0,0 +1,6 @@
+public class ATMTest {
+    public static void main(String[] args) {
+        ATMAccount chuong = new ATMAccount("chuong",10000);
+        chuong.login();
+    }
+}
